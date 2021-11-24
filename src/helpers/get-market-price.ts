9220c47a6b9b48d75733fbe2b249fee20ee62c8c
DoguD <dogudeniz.ugur@gmail.com>
2021-11-24 03:12:29 +0300

    Market Price fix

--- src/helpers/get-market-price.ts
+++ src/helpers/get-market-price.ts
@@ -4,14 +4,10 @@ import { daiAmp } from "../helpers/bond";
 import { Networks } from "../constants/blockchain";
 
 export async function getMarketPrice(networkID: Networks, provider: ethers.Signer | ethers.providers.Provider): Promise<number> {
-    console.log("Inside get market price");
     const daiAMPAddress = daiAmp.getAddressForReserve(networkID);
-    console.log("a1");
     const pairContract = new ethers.Contract(daiAMPAddress, LpReserveContract, provider);
-    console.log("a2");
     const reserves = await pairContract.getReserves();
-    console.log("a3");
-    const marketPrice = reserves[0] / reserves[1];
-    console.log("a4");
+    // TODO: you changed here
+    const marketPrice = reserves[1] / reserves[0];
     return marketPrice;
 }
