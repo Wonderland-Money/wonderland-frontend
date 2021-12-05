04970154a675d7490f525f3edb6be5869ae7826a
DoguD <dogudeniz.ugur@gmail.com>
2021-12-06 01:46:31 +0300

    Header Logo Change

--- src/views/Landing/components/Header/index.tsx
+++ src/views/Landing/components/Header/index.tsx
@@ -18,7 +18,7 @@ function Header() {
 
     return (
         <div className="landing-header">
-            <SvgIcon color="primary" component={WonderlandIcon} viewBox="0 0 174 40" style={{ minWidth: 174, minHeight: 40 }} />
+            <img src={"cupcake.png"} style={{ height: 60 }} />
             <div className="landing-header-nav-wrap">
                 <Box component="div" onMouseEnter={e => handleClick(e)} onMouseLeave={e => handleClick(e)}>
                     <p className="landing-header-nav-text">Social</p>
