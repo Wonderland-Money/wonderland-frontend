cc2b503d6c4fc5984e9b52d13f9f05f30b732329
MaazV <71987517+MaazV@users.noreply.github.com>
2021-12-18 13:10:46 -0500

    updated contract addresses and abis

--- src/helpers/get-market-price.ts
+++ src/helpers/get-market-price.ts
@@ -1,12 +1,12 @@
 import { ethers } from "ethers";
 import { LpReserveContract } from "../abi";
-import { mimPsi } from "../helpers/bond";
+import { fraxPsi } from "../helpers/bond";
 import { Networks } from "../constants/blockchain";
 
 export async function getMarketPrice(networkID: Networks, provider: ethers.providers.Provider | ethers.Signer): Promise<number> {
-    const mimPsiAddress = mimPsi.getAddressForReserve(networkID);
+    const fraxPsiAddress = fraxPsi.getAddressForReserve(networkID);
     // @ts-ignore
-    const pairContract = new ethers.Contract(mimPsiAddress, LpReserveContract, provider);
+    const pairContract = new ethers.Contract(fraxPsiAddress, LpReserveContract, provider);
     const reserves = await pairContract.getReserves();
     const marketPrice = reserves[0] / reserves[1];
     return marketPrice;
